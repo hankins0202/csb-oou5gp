268d392279cedb957787f872a79c6f874835691d
Code Sandbox <hello@codesandbox.io>
2022-03-18 21:35:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dc94f6d2c6b0cd8fb28d3edbde602cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd994e2c3a3f1fcf20e25a7d8693b89",
+    "url": "https://hankins0202.github.io/csb-oou5gp/index.html"
+  },
+  {
+    "revision": "e04ee2cc8ce0c73f2178",
+    "url": "https://hankins0202.github.io/csb-oou5gp/static/css/2.156fe21f.chunk.css"
+  },
+  {
+    "revision": "e04ee2cc8ce0c73f2178",
+    "url": "https://hankins0202.github.io/csb-oou5gp/static/js/2.f166dad5.chunk.js"
+  },
+  {
+    "revision": "b74035b2e1bdf38860de",
+    "url": "https://hankins0202.github.io/csb-oou5gp/static/js/main.55b26ef5.chunk.js"
+  },
+  {
+    "revision": "07c09dcaa1da48e93aca",
+    "url": "https://hankins0202.github.io/csb-oou5gp/static/js/runtime~main.b818b370.js"
+  },
+  {
+    "revision": "849fa99ccaef12ec3f044d9682026a01",
+    "url": "https://hankins0202.github.io/csb-oou5gp/static/media/metal.849fa99c.jpeg"
+  }
+]);
